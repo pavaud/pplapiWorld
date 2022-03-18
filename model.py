49d48aa1996823cf29b3297b16aeee9e42db73af
Philippe Vaudin <philippe.vaudin@gmail.com>
2022-03-18 20:38:05 +0100

    Add class Position, Zone

--- model.py
+++ model.py
@@ -1,7 +1,58 @@
+import json
+import math
+
 class Agent:
     
-    def __init__(self, agreeableness):
-        self.agreeableness = agreeableness
+    def __init__(self, position, **agent_attributes):
+        self.position = position
+        for attr_name, attr_value in agent_attributes.items():
+            setattr(self, attr_name, attr_value)
 
-first_agent = Agent(0)
-print(first_agent.agreeableness)
+class Position:
+    def __init__(self, longitude_degrees, latitude_degrees):
+        self.latitude_degrees = latitude_degrees
+        self.longitude_degrees = longitude_degrees
+        # elf.longitude = self.longitude_degrees * math.pi / 180
+
+    @property
+    def longitude(self):
+        return self.longitude_degrees * math.pi / 180
+
+class Zone:
+
+    MIN_LONGITUDE_DEGREES = -180
+    MAX_LONGITUDE_DEGREES = 180
+    MIN_LATITUDE_DEGREES = -90
+    MAX_LATITUDE_DEGREES = 90
+    WIDTH_DEGREES = 1 # degrees of longitude
+    HEIGHT_DEGREES = 1 # degrees of latitude
+    ZONES = []
+    
+    def __init__(self, corner1, corner2):
+        self.corner1 = corner1
+        self.corner2 = corner2
+        self.inhabitants = 0
+    
+    @classmethod # Permet de dire que cette methode peut être utiliser sans instance
+    def initialize_zones(cls):
+        for latitude in range (cls.MIN_LATITUDE_DEGREES, cls.MAX_LATITUDE_DEGREES, cls.HEIGHT_DEGREES):
+            for longitude in range(cls.MIN_LONGITUDE_DEGREES, cls.MAX_LONGITUDE_DEGREES, cls.WIDTH_DEGREES):
+                bottom_left_corner = Position(latitude, longitude)
+                top_right_corner = Position(longitude + cls.WIDTH_DEGREES, latitude + cls.HEIGHT_DEGREES)
+                zone = Zone(bottom_left_corner, top_right_corner)
+                cls.ZONES.append(zone)
+        print(len(cls.ZONES))
+            
+def main():
+    '''
+    for agent_attributes in json.load(open("agents-100k.json")):
+        latitude = agent_attributes.pop("latitude")
+        longitude = agent_attributes.pop("longitude")
+        position = Position(longitude, latitude)
+        agent = Agent(position, **agent_attributes)
+        print(agent.position.longitude)
+    '''
+    Zone.initialize_zones()
+        
+if __name__ == '__main__':
+    main()
